32056b87d193819babb5946d5a6fa55dc03023c6
Vasilii Kamenets <resecmo@gmail.com>
2020-12-24 22:57:44 +0300

    added time normalization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import datetime
 import math as m
 import sys
-from time import sleep
+import time
 
 import pygame
 vec2 = pygame.math.Vector2
@@ -21,7 +22,8 @@ pygame.display.set_caption("CurvedRaces")
 points = [Pos(350, 250), Pos(0, 0), Pos(200, 200), Pos(300, 300)]
 player_point = points[0]
 speed = Pos(0, 0, 0)
-acceleration = 0.1
+acceleration = 900
+angular_acceleration = 8.0
 
 
 def transform(center, point):
@@ -36,7 +38,13 @@ def draw_point(surface, pos):
                      (pos.x - radius * m.sin(pos.phi), pos.y - radius * m.cos(pos.phi)))
 
 
+old_time = datetime.datetime.now()
 while True:
+    new_time = datetime.datetime.now()
+    frame_time = (new_time - old_time).microseconds / 1000000
+    old_time = new_time
+    print(frame_time, type(frame_time))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -46,26 +54,25 @@ while True:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] or keys[pygame.K_w]:
-        speed.x -= acceleration * m.sin(points[0].phi)
-        speed.y -= acceleration * m.cos(points[0].phi)
+        speed.x -= acceleration * m.sin(points[0].phi) * frame_time
+        speed.y -= acceleration * m.cos(points[0].phi) * frame_time
     if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        speed.x += acceleration * m.sin(points[0].phi)
-        speed.y += acceleration * m.cos(points[0].phi)
+        speed.x += acceleration * m.sin(points[0].phi) * frame_time
+        speed.y += acceleration * m.cos(points[0].phi) * frame_time
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        speed.phi += 0.001
+        speed.phi += angular_acceleration * frame_time
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        speed.phi -= 0.001
+        speed.phi -= angular_acceleration * frame_time
     if keys[pygame.K_SPACE]:
         player_point = Pos(200, 200)
         points[0] = player_point
 
-    speed *= 0.99
-    points[0] += speed
+    speed *= 0.9 ** frame_time
+    points[0] += speed * frame_time
 
     screen.fill(black)
     for p in points:
-        print(f"{p}")
         draw_point(screen, Pos(width / 2, height / 2, 0) + transform(points[0], p))
 
-    sleep(0.005)
+    time.sleep(0.005)
     pygame.display.flip()
