7aaea6f48f180f540e27bce82b6bf69ef93755cd
Vasilii Kamenets <resecmo@gmail.com>
2020-12-24 21:44:59 +0300

    Added rotation, REALLY BROKEN for now

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import math as m
 import sys
 from time import sleep
 
 import pygame
 vec2 = pygame.math.Vector2
+
+import position
+Pos = position.Position
 
 
 pygame.init()
@@ -14,17 +18,22 @@ screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 pygame.display.set_caption("CurvedRaces")
 
 
-points = [vec2(350, 250), vec2(0, 0), vec2(200, 200), vec2(300, 300)]
+points = [Pos(350, 250), Pos(0, 0), Pos(200, 200), Pos(300, 300)]
 player_point = points[0]
-speed = vec2(0, 0)
+speed = Pos(0, 0, 0)
+acceleration = 0.1
 
 
 def transform(center, point):
-    return point - center
+    result = point - center
+    return result
 
 
 def draw_point(surface, pos):
-    pygame.draw.circle(surface, (255, 255, 255), pos, 8)
+    radius = 8
+    pygame.draw.circle(surface, (255, 255, 255), (pos.x, pos.y), radius)
+    pygame.draw.line(surface, (255, 0, 0), (pos.x, pos.y),
+                     (pos.x + radius * m.sin(pos.phi), pos.y - radius * m.cos(pos.phi)))
 
 
 while True:
@@ -37,23 +46,26 @@ while True:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] or keys[pygame.K_w]:
-        speed[1] -= 0.1
+        speed.x += acceleration * m.sin(points[0].phi)
+        speed.y += acceleration * m.cos(points[0].phi)
     if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        speed[1] += 0.1
+        speed.x -= acceleration * m.sin(points[0].phi)
+        speed.y -= acceleration * m.cos(points[0].phi)
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        speed[0] -= 0.1
+        speed.phi += 0.001
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        speed[0] += 0.1
+        speed.phi -= 0.001
     if keys[pygame.K_SPACE]:
-        player_point = vec2(200, 200)
+        player_point = Pos(200, 200)
         points[0] = player_point
 
-    speed *= 0.995
-    player_point += speed
+    speed *= 0.99
+    points[0] += speed
 
     screen.fill(black)
+    print(transform(points[0], points[0]).phi)
     for p in points:
-        draw_point(screen, vec2(width / 2, height / 2) + transform(player_point, p))
+        draw_point(screen, Pos(width / 2, height / 2, 0) + transform(points[0], p))
 
     sleep(0.005)
     pygame.display.flip()
---
+++ position.py
@@ -0,0 +1,29 @@
+import math as m
+
+
+class Position:
+    def __init__(self, x, y, phi=0):
+        self.x = x
+        self.y = y
+        self.phi = phi
+
+    def __add__(self, other):
+        if isinstance(other, Position):
+            return Position(self.x + other.x, self.y + other.y, self.phi + other.phi)
+        raise NotImplementedError
+
+    def __sub__(self, other):
+        if isinstance(other, Position):
+            result = Position(self.x - other.x, self.y - other.y, self.phi)
+            result.rotate(-other.phi)
+            return result
+        raise NotImplementedError
+
+    def __mul__(self, other):
+        if type(other) == float:
+            return Position(self.x * other, self.y * other, self.phi * other)
+        raise NotImplementedError
+
+    def rotate(self, psi):
+        self.x, self.y = self.x * m.cos(psi) - self.y * m.sin(psi), self.x * m.sin(psi) + self.y * m.cos(psi)
+        self.phi += psi
